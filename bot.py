4c688ac030a58c64aa159050f7199fa185257c40
Adammanc0 <cassidy.adam.2011@gmail.com>
2026-03-30 21:03:28 +0100

    Add files via upload

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 import os
+import asyncio
+
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="|", intents=intents)
@@ -14,5 +16,13 @@ async def on_ready():
 @bot.tree.command(name="hello", description="Say hello")
 async def hello(interaction: discord.Interaction):
     await interaction.response.send_message("Hello!")
+
+@bot.command()
+async def burst(ctx, *, text: str):
+    for i in range(5):
+        await ctx.send(text)
+        await asyncio.sleep(1)
+
+
 
 bot.run(os.getenv("TOKEN"))
