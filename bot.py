4e80c29e8683626cd1e573d34219d1203e221e95
Adammanc0 <cassidy.adam.2011@gmail.com>
2026-04-03 16:53:06 +0100

    Add files via upload

--- bot.py
+++ bot.py
@@ -625,7 +625,7 @@ async def roastspam(interaction: discord.Interaction, user: discord.User, amount
 # ============================================================
 # Rick roll
 # ============================================================
-@bot.tree.command(name="rickroll", description="Send a mysterious musical message to someone.")
+@bot.tree.command(name="rickroll", description="Flood the chat with the ultimate rick roll.")
 @app_commands.allowed_installs(guilds=True, users=True)
 @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
 @app_commands.describe(user="The user to rickroll")
@@ -647,7 +647,7 @@ A full commitment's what I'm thinking of
 You wouldn't get this from any other guy
 I just wanna tell you how I'm feeling
 Gotta make you understand
-Never gonna give you up
+Never gonna give you up {user.mention}
 Never gonna let you down
 Never gonna run around and desert you
 Never gonna make you cry
@@ -659,7 +659,7 @@ Inside we both know what's been going on
 We know the game and we're gonna play it
 And if you ask me how I'm feeling
 Don't tell me you're too blind to see
-Never gonna give you up
+Never gonna give you up {user.mention}
 Never gonna let you down
 Never gonna run around and desert you
 Never gonna make you cry
@@ -671,13 +671,13 @@ No, I'll never run around and hurt you
 Never, ever desert you
 We've known each other for so long
 Your heart's been aching but
-Never gonna give you up
+Never gonna give you up {user.mention}
 Never gonna let you down
 Never gonna run around and desert you
 Never gonna make you cry
 Never gonna say goodbye
 Never gonna tell a lie and hurt you
-No, I'm never gonna give you up
+No, I'm never gonna give you up {user.mention}
 No, I'm never gonna let you down
 No, I'll never run around and hurt you
 I'll never, ever desert you**"""
